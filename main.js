5c5f71fa2cf841fc0eb19a719ad7e2c0c3c85fbf
alexiscg0162-coder <alexiscastaneda0162@politecnicomayor.edu.co>
2025-09-20 11:50:18 -0500

    Add files via upload
    ACTUALIZACION

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 document.addEventListener('DOMContentLoaded', (e)=> {
     const carrito = document.getElementById('carrito')
     carrito.addEventListener('mouseover', ()=> {
-        console.log('Pasaste por encima del carrito'); 
-        setTimeout( () => {
-            const mostarCarrito = document.querySelector('.mostrar-carrito')
-            mostarCarrito.style.display = "block"; 
+            console.log('Pasaste por encima del carrito'); 
+            setTimeout( () => {
+             const mostrarCarrito = document.querySelector('.mostrar-carrito')
+             mostrarCarrito.style.display = "block"; 
         })
     }); 
 
-        carrito.addEventListener('mouseleave', () => {
+        carrito.addEventListener('mouseleave', ()=> {
             console.log('Pasaste por encima del carrito'); 
             const mostrarCarrito = document.querySelector('.mostrar-carrito')
             mostrarCarrito.style.display = "none"; 
-        })
-},3000); 
+        }); 
+},3000);
 
 const botones = document.querySelectorAll('.btn-primary'); 
-botones.forEach( () => {
-    botones.addEventListener('click', ()=> {
-        setTimeout( ()=> {
-            botones.textContent = "Agregando al carrito"; 
-            botones.classList.remove('.btn-primary'); 
-        })
-    })
+botones.forEach( (boton) => {
+    boton.addEventListener("click", (e)=> {
+        setTimeout( () => {
+            boton.classList.remove("btn-primary"); 
+            boton.textContent = "Agregando al carrito"; 
+            boton.classList.add("btn-info"); 
+        },1000)
+
+        setTimeout(() => {
+            boton.classList.remove("btn-info"); 
+            boton.textContent = "Agregado al carrito"; 
+            boton.classList.add("btn-primary"); 
+        },3000)
+
+        setTimeout(() => {
+            boton.textContent = "Agregar al carrito"; 
+        },5000)
+
+        const productos = e.target.closest('.productos');
+        console.log(productos);
+    }); 
+
 })
